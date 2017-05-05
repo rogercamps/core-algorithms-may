acbc62439e3d0f9c577026e320478ea4a72d41c5
rogercamps <rogercamps@me.com>
2017-05-05 16:48:42 -0700

    modify collatz test

--- spec/collatzConjecture_test.js
+++ spec/collatzConjecture_test.js
@@ -1,8 +1,8 @@
-import { expect } from 'chai'
-import collatzConjecture from '../src/collatzConjecture'
-
-describe('collatzConjecture()', function(){
-
-  it('should be a function', function(){
-    expect(collatzConjecture).to.be.a('function')
-  })
+// import { expect } from 'chai'
+// import collatzConjecture from '../src/collatzConjecture'
+//
+// describe('collatzConjecture()', function(){
+//
+//   it('should be a function', function(){
+//     expect(collatzConjecture).to.be.a('function')
+//   })
